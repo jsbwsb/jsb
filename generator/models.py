6507040e39fde7998266802b3d6e3c5cebd99e02
jsbwsb <jsb.wsb@gmail.com>
2015-05-13 22:54:11 +0200

    Step 1: Wojewodzctwo

--- generator/models.py
+++ generator/models.py
@@ -76,9 +76,15 @@ def get_option_value(req, step):
     elif step == 3:
         ret = [-1]
 
+
         if req_dict.has_key('options'):
 
-            ret = req_dict['options']
+
+            options_list = list(req_dict['options'])
+        else:
+            options_list = [[-1, ['Wszystkie']]]
+
+        ret = options_list
 
     elif step == 4:
         ret = req
