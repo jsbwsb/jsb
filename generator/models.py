930042164aa99ea60722dc7b3bfe2dcd835d2e0d
jsbwsb <jsb.wsb@gmail.com>
2015-05-19 20:31:07 +0200

    Step 1: Wojewodzctwo

--- generator/models.py
+++ generator/models.py
@@ -139,8 +139,8 @@ def get_option_value(req, step):
 
         #wojewodztwa
         if 'options' in req_dict:
-            options_list = [-1, ['Wszystkie']]
-            #options_list = str_to_list(req_dict['options'][0])
+            #options_list = [-1, ['Wszystkie']]
+            options_list = str_to_list(req_dict['options'][0])
         else:
             options_list = [-1, ['Wszystkie']]
 
