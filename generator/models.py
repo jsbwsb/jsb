1c4981128b218618f7161c40402628112431e185
jsbwsb <jsb.wsb@gmail.com>
2015-05-26 22:01:15 +0200

    Step 1: Wojewodzctwo

--- generator/models.py
+++ generator/models.py
@@ -377,6 +377,12 @@ def generate_sql_file(filepath, data, structure=[], number=100):
     # Save (commit) the changes
     conn.commit()
 
+    # Insert a row of data
+    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+    # Save (commit) the changes
+    conn.commit()
+
     # We can also close the connection if we are done with it.
     # Just be sure any changes have been committed or they will be lost.
     conn.close()
