71c666e79727cd1f87b24e4b781216074844b87b
jsbwsb <jsb.wsb@gmail.com>
2015-05-28 19:22:08 +0200

    Step 1: Wojewodzctwo

--- generator/models.py
+++ generator/models.py
@@ -619,14 +619,13 @@ def generate_file(req):
 
         count = 0
         result_nr_domu = []
-        index_nr_domu = rek_struktura.index('nr_domu')
-        max_nr_domu = random.randint(1, 1000)
-        delta_nr_domu = random.randint(1, 4) + 1
-        '''
+        index_nr_domu = int(rek_struktura.index('nr_domu'))
+
         for r in result:
-            max_nr_domu = random.randint(1000)
-            delta_nr_domu = random.randint(4) + 1
-        '''
+            max_nr_domu = random.randint(1, 1000)
+            delta_nr_domu = random.randint(1, 4) + 1
+            r.insert(index_nr_domu, 10)
+
 
 
     filename = str(req_dict['file_name'][0])
